25e720e39da8bde704e33654ae454385fe6a999d
Vishesh Gupta <vishesh.gupta12@outlook.com>
2018-02-03 15:30:24 -0500

    Update zombiefactory.sol

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -15,6 +15,8 @@ contract ZombieFactory is Ownable {
       uint dna;
       uint32 level;
       uint32 readyTime;
+      uint16 winCount;
+      uint16 lossCount;
     }
 
     Zombie[] public zombies;
@@ -23,7 +25,7 @@ contract ZombieFactory is Ownable {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
