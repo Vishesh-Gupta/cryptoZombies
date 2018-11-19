61bc0041448fba804c71e796e79245bdbda76f5d
Vishesh Gupta <vishesh.gupta12@outlook.com>
2018-11-18 20:28:34 -0500

    Added changes to player stats

--- contract files/examplegame.sol
+++ contract files/examplegame.sol
@@ -12,5 +12,16 @@ contract ExampleGame is ZBGameMode  {
         ZBSerializer.SerializedGameStateChanges memory changes;
         changes.init();
 
+        changes.changePlayerDefense(Player.Player1, 15);
+        changes.changePlayerDefense(Player.Player2, 15);
+
+        changes.changePlayerCurrentGooVials(Player.Player1, 3);
+        changes.changePlayerCurrentGooVials(Player.Player2, 3);
+        
+        changes.changePlayerCurrentGoo(Player.Player1, 3);
+        changes.changePlayerCurrentGoo(Player.Player2, 3);
+        
+        changes.changePlayerMaxGooVials(Player.Player1, 8);
+        changes.changePlayerMaxGooVials(Player.Player2, 8);
     }
 }
