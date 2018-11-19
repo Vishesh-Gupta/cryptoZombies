6e9a7847c2c68ad6aef7349b36e4159149527f84
Vishesh Gupta <vishesh.gupta12@outlook.com>
2018-11-19 00:31:26 -0500

    Updated the cards and swapped them to actually load the proper card decks

--- contract files/game-modes/zombiepile.sol
+++ contract files/game-modes/zombiepile.sol
@@ -17,29 +17,25 @@ contract ZombiePile is ZBGameMode  {
         uint player1CardCount = 0;
         uint player2CardCount = 0;
 
-
         for (uint i = 0; i < gameState.playerStates.length; i++) {
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
                 uint rand = uint(keccak256(abi.encodePacked(now, player1CardCount + player2CardCount))) % 2;
-                
                 if (player1CardCount + 1 > gameState.playerStates[0].cardsInDeck.length) {
-                    rand = 1;
+                  rand = 1;
                 } else if (player2CardCount + 1 > gameState.playerStates[1].cardsInDeck.length) {
-                    rand = 0;
+                  rand = 0;
                 }
-                
                 if (rand == 0) {
-                    player1Cards[player1CardCount] = gameState.playerStates[i].cardsInDeck[j];
-                    player1CardCount++;
+                   player1Cards[player1CardCount] = gameState.playerStates[i].cardsInDeck[j];
+                   player1CardCount++;
                 } else {
-                    player2Cards[player2CardCount] = gameState.playerStates[i].cardsInDeck[j];
-                    player2CardCount++;
+                   player2Cards[player2CardCount] = gameState.playerStates[i].cardsInDeck[j];
+                   player2CardCount++;
                 }
             }
-
-            changes.changePlayerCardsInDeck(Player(i), newCards, cardCount);
         }
-
+        changes.changePlayerCardsInDeck(Player.Player1, player1Cards, player1CardCount);
+        changes.changePlayerCardsInDeck(Player.Player2, player2Cards, player2CardCount);
         changes.emit();
 
     }
