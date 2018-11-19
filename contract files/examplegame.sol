014afc91eb5eab1a63175e947826ef9caba9c206
Vishesh Gupta <vishesh.gupta12@outlook.com>
2018-11-18 21:59:21 -0500

    Modifying Players' Decks

--- contract files/examplegame.sol
+++ contract files/examplegame.sol
@@ -4,6 +4,36 @@ import "./ZB/ZBGameMode.sol";
 
 contract ExampleGame is ZBGameMode  {
 
+    // 1. Declare mapping here
+    mapping (string => bool) internal bannedCards;
+
+    // 2. Create constructor here
+    constructor() public {
+        bannedCards["Leash"] = true;
+        bannedCards["Bulldozer"] = true;
+        bannedCards["Lawnmower"] = true;
+        bannedCards["Shopping Cart"] = true;
+        bannedCards["Stapler"] = true;
+        bannedCards["Nail Bomb"] = true;
+        bannedCards["Goo Bottles"] = true;
+        bannedCards["Molotov"] = true;
+        bannedCards["Super Goo Serum"] = true;
+        bannedCards["Junk Spear"] = true;
+        bannedCards["Fire Extinguisher"] = true;
+        bannedCards["Fresh Meat"] = true;
+        bannedCards["Chainsaw"] = true;
+        bannedCards["Bat"] = true;
+        bannedCards["Whistle"] = true;
+        bannedCards["Supply Drop"] = true;
+        bannedCards["Goo Beaker"] = true;
+        bannedCards["Zed Kit"] = true;
+        bannedCards["Torch"] = true;
+        bannedCards["Shovel"] = true;
+        bannedCards["Boomstick"] = true;
+        bannedCards["Tainted Goo"] = true;
+        bannedCards["Corrupted Goo"] = true;
+    }
+
     function beforeMatchStart(bytes serializedGameState) external {
 
         GameState memory gameState;
@@ -12,24 +42,19 @@ contract ExampleGame is ZBGameMode  {
         ZBSerializer.SerializedGameStateChanges memory changes;
         changes.init();
 
-        // changePlayerCurrentGooVials — the number of vials the player currently has available for this round. The default starts at 1 when the match starts, and increases by 1 with every turn.
-        // changePlayerCurrentGoo — the number of vials that are filled with goo in the current round. Defaults to be the same as the number of goo vials at the start of each turn.
-        // changePlayerMaxGooVials — the max number of goo vials a player can have in the game. Default is 10.
-        // changePlayerInitialCardsInHandCount — the number of cards the player is dealt when they start the match. Default is 3.
-        // changePlayerMaxCardsInPlay — the maximum number of zombies a player can place on the field. Default is 6.
-        // changePlayerMaxCardsInHand — the maximum number of cards a player can hold in their hand at one time. Default is 10
-        // These functions all follow the same format as changePlayerDefense and accept two arguments: A Player enum, and a uint8.
-
         changes.changePlayerDefense(Player.Player1, 15);
         changes.changePlayerDefense(Player.Player2, 15);
 
         changes.changePlayerCurrentGooVials(Player.Player1, 3);
         changes.changePlayerCurrentGooVials(Player.Player2, 3);
-        
+
         changes.changePlayerCurrentGoo(Player.Player1, 3);
         changes.changePlayerCurrentGoo(Player.Player2, 3);
-        
+
         changes.changePlayerMaxGooVials(Player.Player1, 8);
         changes.changePlayerMaxGooVials(Player.Player2, 8);
+
+
+
     }
 }
