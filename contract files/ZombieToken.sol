0ee97f1629ead11cf86a09e0b4b88f15e61674a2
Vishesh Gupta <vishesh.gupta12@outlook.com>
2018-12-17 19:37:16 -0500

    Added name and symbol method for the Token

--- contract files/ZombieToken.sol
+++ contract files/ZombieToken.sol
@@ -2,6 +2,16 @@ pragma solidity ^0.4.25;
 
 import "./ERC721XToken.sol";
 
+// Our own token based on ERC721x Token
 contract ZombieCard is ERC721XToken {
+    // name method that returns the token name to wallets/exchanges
+    function name() external view returns (string) {
+        return "ZombieCard";
+    }
+
+    // Symbol of the token on wallets / marketplaces / exchanges
+    function symbol() external view returns (string) {
+        return "ZCX";
+    }
     
 }
